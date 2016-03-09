14118423c4384334c41cd505ca62268a42404543
guuba <guuba1016@gmail.com>
2016-03-10 00:29:55 +0800

    * 實作一個清單頁面，顯示欄位有「客戶名稱、聯絡人數量、銀行帳戶數量」共三個欄位，此功能要在資料庫中建立一個檢視表，並且匯入 EDMX。

---
+++ MVCHomeWork1/Controllers/ClientInfoController.cs
@@ -0,0 +1,20 @@
+﻿using MVCHomeWork1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHomeWork1.Controllers
+{
+    public class ClientInfoController : Controller
+    {
+
+        private 客戶資料Entities db = new 客戶資料Entities();
+        // GET: ClientInfo
+        public ActionResult Index()
+        {
+            return View(db.客戶資訊檢視表.ToList());
+        }
+    }
+}
